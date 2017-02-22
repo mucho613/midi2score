ca1a1d9f4ba69f2c9bfcb3c80580b45efe1dc231
Daisuke Yamamura <mucho613@gmail.com>
2017-02-23 00:12:24 +0900

    First commit

---
+++ script.js
@@ -0,0 +1,133 @@
+var midi = null;
+var pro;
+var inputs = [];
+var outputs = [];
+var scoreCanvas = document.getElementById("score");
+var ctx;
+var noteState = new Array(128);
+
+var imgGClef = new Image();
+imgGClef.src = "C:/Users/Mucho/Desktop/web_midi/Gclef.svg";
+
+addEventListener("load", init);
+
+function init(){
+	for(i=0; i<128; i++){
+		noteState[i] = false;
+	}
+
+	ctx = scoreCanvas.getContext('2d');
+	requestAnimationFrame(draw);
+}
+
+function draw(){
+	// ˆê’U‘S•”ƒNƒŠƒA
+	ctx.beginPath();
+	ctx.clearRect(0, 0, scoreCanvas.width, scoreCanvas.height);
+
+	// ŒÜü•ˆ‚ð•`‰æ
+	ctx.beginPath();
+	for(i=0; i<11; i++){
+	    if(i!=5){
+		  ctx.moveTo(20, 20 + (10 * i));
+		  ctx.lineTo(240, 20 + (10 * i));
+	    }
+	}
+	ctx.stroke();
+
+	// ‰¹•„‚ð•`‰æ
+	drawNote();
+
+	// ƒg‰¹‹L†‚ð•`‰æ
+	ctx.drawImage(imgGClef, 30, 0, 30, 80);
+
+	requestAnimationFrame(draw);
+}
+
+var y_pos;
+
+function drawNote(){
+	for(i=0; i<128; i++){
+		// ‰¹•„‚Ìc‚ÌÀ•W‚ðŒvŽZ‚·‚é
+		switch(i % 12) {
+		    case 0:
+		    case 1:
+			y_pos = 0;
+			break;
+		    case 2:
+		    case 3:
+			y_pos = 1;
+			break;
+		    case 4:
+			y_pos = 2;
+			break;
+		    case 5:
+		    case 6:
+			y_pos = 3;
+			break;
+		    case 7:
+		    case 8:
+			y_pos = 4;
+			break;
+		    case 9:
+		    case 10:
+			y_pos = 5;
+			break;
+		    case 11:
+			y_pos = 6;
+			break;
+		}
+		y_pos += Math.floor(i / 12) * 7;
+		if(noteState[i] == true){
+			if(i > 0 && noteState[i - 1] == false){
+			    ctx.beginPath();
+			    ctx.ellipse(130, 245 - (y_pos * 5), 6, 8, 70* Math.PI/180, 0, 2 * Math.PI);
+			    ctx.fill();
+			}
+			else if(i > 1 && noteState[i - 1] == true && noteState[i - 2] == true){
+			    ctx.beginPath();
+			    ctx.ellipse(120, 70 - (y_pos * 5), 6, 8, 70* Math.PI/180, 0, 2 * Math.PI);
+			    ctx.fill();
+			}
+			else if(i > 0 && noteState[i - 1] == true){
+			    ctx.beginPath();
+			    ctx.ellipse(130, 70 - (y_pos * 5), 6, 8, 70* Math.PI/180, 0, 2 * Math.PI);
+			    ctx.fill();
+			}
+
+		}
+	}
+}
+
+promise = navigator.requestMIDIAccess();
+promise.then(successCallback, errorCallback);
+function successCallback(m) {
+	midi = m;
+	var it = midi.inputs.values();
+	for(var o = it.next(); !o.done; o = it.next()){
+		inputs.push(o.value);
+	}
+	var ot = midi.outputs.values();
+	for(var o = ot.next(); !o.done; o = ot.next()){
+		outputs.push(o.value);
+	}
+	for(var cnt=0;cnt < inputs.length;cnt++){
+		inputs[cnt].onmidimessage = onMIDIEvent;
+	}
+}
+function errorCallback() {
+	console.log("Error!");
+}
+function onMIDIEvent(e){
+	if(e.data[0] == 144 && e.data[2] != 0){
+		console.log(e.data[1] + " ”Ô‚ÌŒ®”Õ‚ª‰Ÿ‚³‚ê‚½");
+		noteState[e.data[1]] = true;
+	}
+	if(e.data[0] == 144 && e.data[2] == 0){
+		noteState[e.data[1]] = false;
+	}
+	if(e.data[0] == 128){
+		console.log(e.data[1] + " ”Ô‚ÌŒ®”Õ‚ª—£‚³‚ê‚½");
+		noteState[e.data[1]] = false;
+	}
+}
